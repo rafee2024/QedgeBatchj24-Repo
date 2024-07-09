6f8ca1e4b4f9c3f8ea6f5b48aa8e4085191f582b
cubeiplkumar <emailanilkumarm@gmail.com>
2024-07-09 10:01:14 +0530

    feature201 changes in Application.java file by Vinay

--- Application.java
+++ Application.java
@@ -16,7 +16,28 @@ public class MyClass {
 
         // Accessing the public field
         System.out.println("Public Field: " + myObject.publicField);
+ #feature201 changes by Vinay for the Module PaymentGateway in Jul2024
+ public static void main(String[] args) {
 
+    char[] array = new char[100];
+    try {
+      // Creates a reader using the FileReader
+      FileReader input = new FileReader("input.txt");
+
+      // Reads characters
+      input.read(array);
+      System.out.println("Data in the file:");
+      System.out.println(array);
+
+      // Closes the reader
+      input.close();
+    }
+    catch(Exception e) {
+      e.getStackTrace();
+    }
+  }
+  #End of feature201 code
+  
         // Calling the public method
         myObject.publicMethod();
     }
